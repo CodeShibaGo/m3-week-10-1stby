6f088607379b2358c4e4557b9d775725f9f21d44
1stby <love970120@gmail.com>
2024-04-02 11:40:49 +0800

    Configure MySQL database connection in config.py

--- just-rent-sample-w11/config.py
+++ just-rent-sample-w11/config.py
@@ -7,5 +7,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     POSTS_PER_PAGE = 7
