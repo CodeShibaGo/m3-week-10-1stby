397a7841cd7252af6a59242ec1459b7bdf0284bf
1stby <love970120@gmail.com>
2024-04-02 14:48:00 +0800

    Add new route

--- just-rent-sample-w11/app/controllers/pages_controller.py
+++ just-rent-sample-w11/app/controllers/pages_controller.py
@@ -11,3 +11,19 @@ def home():
 @bp.route('/cars')
 def cars():
     return render_template('cars.html', title='Cars')
+
+@bp.route('/booking')
+def booking():
+    return render_template('booking.html', title='booking')
+
+@bp.route('/my-account')
+def my_account():
+    return render_template('account-profile.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@bp.route('/register')
+def register():
+    return render_template('register.html')
