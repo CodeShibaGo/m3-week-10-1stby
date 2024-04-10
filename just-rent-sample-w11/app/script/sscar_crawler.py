521631066428d98256f6d6576f43baf8487c5e95
Zong <love970120@gmail.com>
2024-04-10 15:47:16 +0800

    feat: Add sscar_crawler.py for car info and short URL crawling

--- just-rent-sample-w11/app/script/sscar_crawler.py
+++ just-rent-sample-w11/app/script/sscar_crawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import  json
 
 def sscar_crawler(url):
     response = requests.get(url)
@@ -12,9 +13,33 @@ def sscar_crawler(url):
         result.append({'name': name, 'url': url})
     return result
 
+def get_yahoo_link(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        link = soup.find_all('h4')[1].find('a')['href']
+        return link
+    except Exception as e:
+        print(f'an error occurred: {e}')
+        return None
+
+
 # 使用範例
 url = "https://sscars.com.tw/car/"
 cars_list = sscar_crawler(url)
 print(cars_list)
+for car in cars_list:
+    url = car['url']
+    if url is not None:
+        car['short_link'] = get_yahoo_link(url)
+print(car)
+
+
+# 轉為json格式
+cars_list_json = json.dumps(cars_list)
+
+#將轉好的json寫入檔案
+with open("car_list.json", "w") as file:
+    file.write(cars_list_json)
 
 
