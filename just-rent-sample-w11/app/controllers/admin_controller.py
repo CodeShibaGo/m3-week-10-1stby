2ed899e615b682763ccaa17ba2c41602dcf1ee34
Zong <love970120@gmail.com>
2024-04-22 16:12:07 +0800

    feat: 建立後台汽車編輯頁面

--- just-rent-sample-w11/app/controllers/admin_controller.py
+++ just-rent-sample-w11/app/controllers/admin_controller.py
@@ -1,7 +1,7 @@
 from sqlalchemy import text
 from app import db
 from app.controllers import bp
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 
 @bp.route('/admin')
@@ -10,4 +10,48 @@ def admin_home():
     result = db.session.execute(sql)
     cars = [row for row in result]
     return render_template('admin/index.html', cars=cars)
+
+
+@bp.route('/admin/edit_car/<int:car_id>', methods=['GET', 'POST'])
+def admin_edit_car(car_id):
+    if request.method == 'POST':
+        db.session.execute(text("""
+            UPDATE cars SET
+            name = :name,
+            displacement = :displacement,
+            body = :body,
+            door = :door,
+            seats = :seats,
+            width = :width,
+            height = :height,
+            Fuel_tank = :Fuel_tank,
+            suitcase = :suitcase
+            WHERE id = :car_id
+        """), {
+            'name': request.form['car_name'],
+            'displacement': request.form['car_displacement'],
+            'body': request.form['car_body'],
+            'door': request.form['car_door'],
+            'seats': request.form['car_seats'],
+            'width': request.form['car_width'],
+            'height': request.form['car_height'],
+            'Fuel_tank': request.form['car_Fuel_tank'],
+            'suitcase': request.form['car_suitcase'],
+            'car_id': car_id
+        })
+
+        db.session.commit()
+        return redirect(url_for('controller.admin_home'))
+
+
+
+    sql = text('SELECT * FROM cars WHERE id = :car_id')
+    result = db.session.execute(sql, {'car_id': car_id})
+    car = result.fetchone()
+
+    return render_template('admin/edit_car.html', car=car)
+
+
+
+
 
