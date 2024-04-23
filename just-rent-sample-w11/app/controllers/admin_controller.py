fd32b5eda2d91149d3eab65568da835936260070
Zong <love970120@gmail.com>
2024-04-23 23:45:54 +0800

    feat: 建立登入畫面，並只有admin能登入

--- just-rent-sample-w11/app/controllers/admin_controller.py
+++ just-rent-sample-w11/app/controllers/admin_controller.py
@@ -1,16 +1,42 @@
+from flask_login import login_required, login_user, logout_user
 from sqlalchemy import text
 from app import db
 from app.controllers import bp
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+
+from app.models.user import User
 
 
 @bp.route('/admin')
+@login_required
 def admin_home():
     sql = text("SELECT * FROM cars")
     result = db.session.execute(sql)
     cars = [row for row in result]
     return render_template('admin/index.html', cars=cars)
 
+@bp.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        sql = text('SELECT * FROM users WHERE username = :username')
+        result = db.session.execute(sql, {'username': username}).fetchone()
+
+        if result and result.password == password:
+            user = User(id=result.id, username=result.username, password=result.password)
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('controller.admin_home'))
+        else:
+            flash('無效的用戶名或密碼')
+    return render_template('admin/login.html')
+
+@bp.route('/admin/logout')
+@login_required
+def admin_logout():
+    logout_user()
+    return redirect(url_for('controller.admin_login'))
 
 @bp.route('/admin/edit_car/<int:car_id>', methods=['GET', 'POST'])
 def admin_edit_car(car_id):
--- just-rent-sample-w11/app/models/user.py
+++ just-rent-sample-w11/app/models/user.py
@@ -1,7 +1,12 @@
+from flask_login import UserMixin
 from app import db
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+
+    def get_id(self):
+        return str(self.id)
