e9ef3f5d138ea9ad043bd1fb752976e2813dfa79
Zong <love970120@gmail.com>
2024-04-24 10:32:09 +0800

    feat: 管理員查看註冊狀況列表

--- just-rent-sample-w11/app/controllers/admin_controller.py
+++ just-rent-sample-w11/app/controllers/admin_controller.py
@@ -10,9 +10,8 @@ from app.models.user import User
 @bp.route('/admin')
 @login_required
 def admin_home():
-    sql = text("SELECT * FROM cars")
-    result = db.session.execute(sql)
-    cars = [row for row in result]
+    query = text("SELECT * FROM cars")
+    cars = db.session.execute(query)
     return render_template('admin/index.html', cars=cars)
 
 @bp.route('/admin/login', methods=['GET', 'POST'])
@@ -20,16 +19,20 @@ def admin_login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        sql = text('SELECT * FROM users WHERE username = :username')
-        result = db.session.execute(sql, {'username': username}).fetchone()
+        query = text('SELECT * FROM users WHERE username = :username')
+        user_data = db.session.execute(query, {'username': username}).fetchone()
 
-        if result and result.password == password:
-            user = User(id=result.id, username=result.username, password=result.password)
-            login_user(user)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('controller.admin_home'))
+        if user_data:
+            sql_password = user_data.password
+            admin = user_data.is_admin
+            if sql_password == password and admin == 1:
+                user = User(id=user_data.id, username=user_data.username, password=user_data.password)
+                login_user(user)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('controller.admin_home'))
         else:
             flash('無效的用戶名或密碼')
+    
     return render_template('admin/login.html')
 
 @bp.route('/admin/logout')
@@ -37,6 +40,13 @@ def admin_login():
 def admin_logout():
     logout_user()
     return redirect(url_for('controller.admin_login'))
+
+@bp.route('/admin/users')
+@login_required
+def admin_users():
+    query = text('SELECT * FROM users')
+    users = db.session.execute(query)
+    return render_template('admin/users.html', users=users)
 
 @bp.route('/admin/edit_car/<int:car_id>', methods=['GET', 'POST'])
 def admin_edit_car(car_id):
@@ -69,16 +79,16 @@ def admin_edit_car(car_id):
         db.session.commit()
         return redirect(url_for('controller.admin_home'))
 
-    sql = text('SELECT * FROM cars WHERE id = :car_id')
-    result = db.session.execute(sql, {'car_id': car_id})
-    car = result.fetchone()
+    query = text('SELECT * FROM cars WHERE id = :car_id')
+    car = db.session.execute(query, {'car_id': car_id}).fetchone()
+
 
     return render_template('admin/edit_car.html', car=car)
 
 @bp.route('/admin/view_car/<int:car_id>')
 def admin_view_car(car_id):
-    sql = text('SELECT * FROM cars WHERE id = :car_id')
-    result = db.session.execute(sql, {'car_id': car_id})
-    car = result.fetchone()
+    query = text('SELECT * FROM cars WHERE id = :car_id')
+    car = db.session.execute(query, {'car_id': car_id}).fetchone()
+
 
     return render_template('admin/view_car.html', car=car)
--- just-rent-sample-w11/app/models/user.py
+++ just-rent-sample-w11/app/models/user.py
@@ -6,7 +6,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-
+    is_admin = db.Column(db.Boolean,default=False)
 
     def get_id(self):
         return str(self.id)
