7ea9023e3a8f9c21ba54d138c93c7501ea3266a5
Zong <love970120@gmail.com>
2024-05-08 14:43:35 +0800

    feat: 訂購車輛的資料庫設計

--- just-rent-sample-w11/app/models/booking.py
+++ just-rent-sample-w11/app/models/booking.py
@@ -1,5 +1,12 @@
 from app import db
 
+class BookingStatus:
+    PENDING = 1 #等待付款
+    CONFIRMED = 2 #以確認
+    CANCELED = 3 #以取消
+    COMPLETED = 4 #完成訂單
+
+
 class Booking (db.Model):
     __tablename__ = 'booking'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +16,8 @@ class Booking (db.Model):
     drop_off_location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
     pick_up_time = db.Column(db.DateTime, nullable=False)
     return_time = db.Column(db.DateTime, nullable=False)
+    Status = db.Column(db.Integer, nullable=False)
+
 
     user = db.relationship('User', backref='bookings')
     car = db.relationship('Car', backref='bookings')    
--- just-rent-sample-w11/app/models/car.py
+++ just-rent-sample-w11/app/models/car.py
@@ -1,6 +1,10 @@
 from app import db
 from app.models.wishlist import Wishlist
 
+class CarStatus:
+    AVAILABLE = 1 #可租
+    BOOKED = 2 #預定中
+    RENTED = 3 #以出租
 
 class Car(db.Model):
     __tablename__ = 'cars'
@@ -15,6 +19,7 @@ class Car(db.Model):
     Fuel_tank = db.Column(db.String(255), nullable=False)
     suitcase = db.Column(db.String(255))
     booking_count = db.Column(db.Integer)
+    Status = db.Column(db.Integer, nullable=False)
 
     wishlists = db.relationship('Wishlist', backref='car', lazy=True)
 
